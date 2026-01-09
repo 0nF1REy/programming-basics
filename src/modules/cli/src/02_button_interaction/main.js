48ab2dd252fdd1db62ef75eff8d876b9629769f3
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-09 03:35:28 -0300

    main: feat (cli/02_button_interaction) - Added HTML, CSS, and JavaScript for button interaction example

---
+++ src/modules/cli/src/02_button_interaction/main.js
@@ -0,0 +1,9 @@
+function helloAlan() {
+  alert("Olá Alan!");
+
+  //   const message = "Olá Alan!";
+  //   const messageElement = document.createElement("div");
+
+  //   messageElement.textContent = message;
+  //   document.body.appendChild(messageElement);
+}
